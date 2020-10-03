bcbe1fd770527f0d3adcd233587c341705851083
sasha mihalache <sasha.mihalache@gmail.com>
2020-10-04 01:01:38 +0300

    create toppings content type and custom previews

---
+++ server/schemas/topping.js
@@ -0,0 +1,36 @@
+import { FaPepperHot as icon } from 'react-icons/fa'
+
+export default {
+  // Computer Name
+  name: 'topping',
+  // Visible Title
+  title: 'Toppings',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Topping Name',
+      type: 'string',
+      description: 'What is the name of the topping?'
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegetarian',
+      type: 'boolean',
+      description: 'What is the name of the topping?',
+      options: {
+        layout: 'checkbox'
+      }
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      vegetarian: 'vegetarian'
+    },
+    prepare: (fields) => ({
+      title: `${fields.name} ${fields.vegetarian ? '🌱' : ''}`
+    })
+  }
+}
