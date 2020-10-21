8f7a9a178d6cd2c79d47502d5bc304d9f3c00d16
sasha mihalache <sasha.mihalache@gmail.com>
2020-10-21 12:38:24 +0300

    learning gatsby queries

--- client/src/pages/pizzas.js
+++ client/src/pages/pizzas.js
@@ -1,5 +1,34 @@
+import { graphql } from 'gatsby';
 import React from 'react';
+import PizzaList from '../components/PizzaList';
 
-export default function PizzasPage() {
-  return <>Hey im the pizzas page</>;
+export default function PizzasPage({ data }) {
+  const pizzas = data.pizzas.nodes;
+
+  return <><PizzaList pizzas={pizzas} /></>;
 }
+
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
