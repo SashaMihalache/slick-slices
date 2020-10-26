b2848ec0ff7c731af1aad5ef73df0c35ade84a5e
sasha mihalache <sasha.mihalache@gmail.com>
2020-10-26 14:13:33 +0200

    templating and styling our single pizza page

--- client/src/components/ToppingFilter.js
+++ client/src/components/ToppingFilter.js
@@ -58,15 +58,8 @@ function countPizzasInToppings(pizzas) {
 export default function ToppingsFilter() {
   // Get a list of all the toppings
   // Get a list of all the Pizzas with their toppings
-  const { toppings, pizzas } = useStaticQuery(graphql`
+  const { pizzas } = useStaticQuery(graphql`
     query {
-      toppings: allSanityTopping {
-        nodes {
-          name
-          id
-          vegetarian
-        }
-      }
       pizzas: allSanityPizza {
         nodes {
           toppings {
--- client/src/templates/Pizza.js
+++ client/src/templates/Pizza.js
@@ -1,8 +1,28 @@
 import { graphql } from 'gatsby';
 import React from 'react';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
+
+const PizzaGrid = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+`;
 
 export default function SinglePizzaPage({ data: { pizza } }) {
-  return <p>Single Pizza</p>;
+  return (
+    <PizzaGrid>
+      <Img fluid={pizza.image.asset.fluid} />
+      <div>
+        <h2 className="mark">{pizza.name}</h2>
+        <ul>
+          {pizza.toppings.map((topping) => (
+            <li key={topping.id}>{topping.name}</li>
+          ))}
+        </ul>
+      </div>
+    </PizzaGrid>
+  );
 }
 
 // this needs to be dynamic based on the slug passed on the context in gatsby-node.js
