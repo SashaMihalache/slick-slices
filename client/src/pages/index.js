8b467fd407dc3cd02341f3ca0532417a9a655b63
sasha mihalache <sasha.mihalache@gmail.com>
2020-11-07 13:13:48 +0200

    creating a skeleton screen while loading items

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import LoadingGrid from '../components/LoadingGrid';
+import { HomePageGrid } from '../styles/Grids';
 import useLatestData from '../utils/useLatestData';
 
-function CurrentlySlicing() {
-  return <div>Currently Slicing</div>;
+function CurrentlySlicing({ slicemasters }) {
+  return (
+    <div>
+      {!slicemasters && <LoadingGrid count={4} />}
+      {slicemasters && !slicemasters?.length && <p>No one is working</p>}
+    </div>
+  );
 }
 
-function HotSlices() {
-  return <div>Hot Slices</div>;
+function HotSlices({ hotSlices }) {
+  return (
+    <div>
+      {!hotSlices && <LoadingGrid count={4} />}
+      {hotSlices && !hotSlices?.length && <p>Nothing in the case</p>}
+    </div>
+  );
 }
 
 export default function HomePage() {
@@ -16,10 +28,10 @@ export default function HomePage() {
     <div className="center">
       <h1>The Best Pizza Downtown!</h1>
       <p>Open 11am to 11pm Every Single Day</p>
-      <div>
+      <HomePageGrid>
         <CurrentlySlicing slicemasters={slicemasters} />
         <HotSlices hotSlices={hotSlices} />
-      </div>
+      </HomePageGrid>
     </div>
   );
 }
