a7aa1b31cd3424fc0d3fd1fe4434f5ce06ecd2b4
sasha mihalache <sasha.mihalache@gmail.com>
2020-11-07 15:19:40 +0200

    responsive header

---
+++ client/public/component---src-pages-404-js-ace0ec87feee23cb1d43.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(n,e,t){"use strict";t.r(e),t.d(e,"default",(function(){return o}));var u=t("q1tI"),a=t.n(u);function o(){return a.a.createElement("p",null,"Hey - That page doesn't exist")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-ace0ec87feee23cb1d43.js.map
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -47,9 +47,33 @@ const NavStyles = styled.nav`
     &:hover {
       color: var(--red);
     }
-    &[aria-current='page'] {
-      color: var(--red);
     }
+  }
+
+  @media(max-width: 600px) {
+    --columns: 4;
+    margin-bottom: 2rem;
+    border-bottom: 1px solid var(--grey);
+    padding-bottom: 2rem;
+
+    ul {
+      grid-template-rows: auto auto;
+      grid-template-columns: repeat(var(--columns), 1fr);
+      justify-items: center;
+    }
+
+    .logo-item {
+      order: 0;
+      grid-column: 1 / -1;
+    }
+
+    .logo {
+      transform: none;
+    }
+  }
+
+  @media(max-width: 500px) {
+    --columns: 2;
   }
 `;
 
@@ -63,7 +87,7 @@ export default function Nav() {
         <li>
           <Link to="/pizzas/">Pizza Menu</Link>
         </li>
-        <li>
+        <li className="logo-item">
           <Logo to="/">LOGO</Logo>
         </li>
         <li>
